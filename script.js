fe0f92b200dffe7c734cde2dfa33861d6387e126
Mariana Pinto <mariana.vendasbb@gmail.com>
2023-03-07 11:41:28 -0300

    declared variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1 - Declarar as variáveis
+
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('.tbody')
+const sNome = document.querySelector('.#m-nome')
+const sFuncao = document.querySelector('.#m-funcao')
+const sSalario = document.querySelector('.#salario')
+const btnSalvar = document.querySelector('.#btnSalvar')
